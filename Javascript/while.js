7ca7606021f7932929361c47ffbd88639e06737c
andika-yudhistira <andikayudhistira.it@gmail.com>
2021-04-20 14:49:13 +0700

    Latihan While

---
+++ Javascript/while.js
@@ -0,0 +1,23 @@
+// var ulang = true;
+// while(ulang) {
+//     console.log('Hello World!');
+//     ulang = confirm('Lagi?');
+// }
+
+// var NilaiAwal = 1
+// while(NilaiAwal <=5){
+//     console.log('Hello World!');
+//     NilaiAwal++
+// }
+
+// var NilaiAwal = 1
+// while(NilaiAwal <=5){
+//     console.log('Hello World! ' + NilaiAwal + 'x');
+//     NilaiAwal++
+// }
+
+var Angkot = 1
+while (Angkot <=10) {
+    console.log('Angkot No. ' + Angkot + ' beroperasi dengan baik.');
+    Angkot++
+}
