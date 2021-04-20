68c8b86c06cb2247fdd5c3421448f575cb105a4b
andika-yudhistira <andikayudhistira.it@gmail.com>
2021-04-20 13:09:19 +0700

    Menambahkan Belajar Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,22 @@
+var x = 10;
+console.log('Hello World!');
+console.log('isi dari variable x adalah ' + x);
+
+// //Pop up Box
+// alert('Hello World!');
+// var nama = prompt('Masukkan nama');
+// confirm('Kamu yakin?');
+// alert('Halo ' + nama);
+
+// Simple Program
+alert('Selamat datang');
+var lagi = true
+
+while( lagi ) {
+    var nama = prompt('Masukkan nama:');
+    alert('Halo ' + nama);
+
+    lagi = confirm('Coba lagi?');
+}
+
+alert('Terimakasih');
